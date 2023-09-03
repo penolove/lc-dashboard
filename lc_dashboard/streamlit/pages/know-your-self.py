45c71f758f513123005e43cb0546a1f369ae8d2f
chinghua.yang <chinghua.yang@appier.com>
2023-09-04 01:49:24 +0800

    update requirements

--- lc_dashboard/streamlit/pages/know-your-self.py
+++ lc_dashboard/streamlit/pages/know-your-self.py
@@ -1,12 +1,16 @@
 import os
 
+import numpy as np
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid, DataReturnMode
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 
-GS_SHEET_ID = os.environ.get("GS_SHEET_ID", "1wADT0jfyHTXAcu5WK3Sa3KGKWaoCwcFZ_sCQqR2XZrY")
+GS_SHEET_ID = os.environ.get(
+    "GS_SHEET_ID", "1wADT0jfyHTXAcu5WK3Sa3KGKWaoCwcFZ_sCQqR2XZrY"
+)
 
 df = pd.read_csv(
     f"https://docs.google.com/spreadsheets/d/{GS_SHEET_ID}/export?format=csv"
@@ -33,26 +37,53 @@ stat = df.groupby(["country", "competition"]).agg(
 )
 
 user_id = st.sidebar.selectbox(
-    "pick your id compare with others", list(set(df["name"])), 
+    "pick your id compare with others",
+    list(set(df["name"])),
     help="leetcode id that wanna analysis",
 )
+
+is_calculate_competitors = st.sidebar.toggle(
+    "If suggest competitors?",
+    help="will pick k candidates who's percentile most close to you",
+)
+if is_calculate_competitors:
+    n_competitors = st.sidebar.number_input(
+        "number of competitors?", min_value=1, max_value=10, value=3
+    )
+    min_competitions = st.sidebar.number_input(
+        "minimum number of competition attended?", min_value=1, max_value=10, value=3
+    )
+
 
 if user_id:
     person_score = df[df["name"] == user_id][
         ["name", "rank", "competition", "percentile"]
     ]
 
-    # pick competitors
+    if is_calculate_competitors:
+        user_median = person_score["percentile"].median()
+        candidates = df.groupby("name").agg(
+            {"percentile": "median", "competition": "count"}
+        )
+        candidates = candidates[candidates["competition"] >= min_competitions]
+        candidates["diff"] = (candidates["percentile"] - user_median).abs()
+        suggest_competitors = set(
+            candidates.nsmallest(n_competitors + 1, "diff").index
+        ) - set([user_id])
 
+    else:
+        suggest_competitors = None
+
+    # pick competitors
     competitors = st.multiselect(
-        "select competitors", set(df["name"])
+        "select competitors", set(df["name"]), suggest_competitors
     )
     # add competitor into dataframe
     for competitor in competitors:
         competitor_score = df[df["name"] == competitor][["rank", "competition"]]
         competitor_score.columns = [competitor, "competition"]
         person_score = person_score.merge(
-            competitor_score, on="competition", how="left"
+            competitor_score, on="competition", how="outer"
         )
 
     merged_result = person_score.merge(stat, on=["competition"])
@@ -63,24 +94,18 @@ if user_id:
         "seventy_five_rank",
     ] + competitors
     selected_lines = st.multiselect(
-        "available platforms", available_lines, available_lines
+        "available metrics", available_lines, available_lines
     )
+    merged_result.sort_values("ts", inplace=True)
 
     # draw lines
-    st.line_chart(
-        merged_result[
-            (
-                [
-                    "ts",
-                    "rank",
-                ]
-                + selected_lines
-            )
-        ],
+    fig = px.line(
+        merged_result[["ts", "rank"] + selected_lines],
         x="ts",
         y=["rank"] + selected_lines,
-        height=500,
+        markers=True,
     )
+    st.plotly_chart(fig, use_container_width=True, theme="streamlit")
 
     # show metrics you compare to others
     st.write(user_id)
@@ -92,6 +117,8 @@ if user_id:
             row[1]["fifty_rank"],
             row[1]["seventy_five_rank"],
         )
+        if np.isnan(user_rank):
+            continue
         col0, col1, col2, col3, col4 = st.columns(5)
         with col0:
             st.write(row[1]["competition"])
