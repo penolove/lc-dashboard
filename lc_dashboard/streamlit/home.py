940429b1fa87e7580da46ac9bf083ec18fe5b5c9
chinghua.yang <chinghua.yang@appier.com>
2023-09-07 01:51:47 +0800

    grammar update

--- lc_dashboard/streamlit/home.py
+++ lc_dashboard/streamlit/home.py
@@ -7,7 +7,7 @@ GS_SHEET_ID = os.environ.get("GS_SHEET_ID", "1wADT0jfyHTXAcu5WK3Sa3KGKWaoCwcFZ_s
 st.header("Look at Leetcode dashboard, Set your goal.")
 
 
-st.write("Hi, here is penolove,")
-st.write(f"this app leetcode data source from a handy maintained google [sheet](https://docs.google.com/spreadsheets/d/{GS_SHEET_ID}/)")
+st.write("Hi, This is penolove,")
+st.write(f"This app leetcode data source from a handy maintained google [sheet](https://docs.google.com/spreadsheets/d/{GS_SHEET_ID}/)")
 
-st.write("feel free to let me know any of your great idea via github [issues](https://github.com/penolove/lc-dashboard/issues).")
+st.write("Feel free to let me know any of your great idea via github [issues](https://github.com/penolove/lc-dashboard/issues).")
